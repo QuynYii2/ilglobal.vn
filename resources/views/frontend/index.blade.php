ce759110dd6a912a876a000d96bdeb087d6e6691
Quy Nguyen <quy.nguyen@Quys-MacBook-Pro.local>
2023-09-13 23:11:31 +0700

    remove swiper js index

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2099,23 +2099,23 @@
         </article>
     </div>
 @endsection
-<script>
-    const showcaseSlider = new Swiper(".home-showcaseSlider", {
-        speed: 1000,
-        slidesPerView: 1,
-        parallax: true,
-        // centeredSlides: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".showcaseSlider-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.showcaseSlider-next',
-            prevEl: '.showcaseSlider-prev',
-        },
-    });
-</script>
+{{--<script>--}}
+{{--    const showcaseSlider = new Swiper(".home-showcaseSlider", {--}}
+{{--        speed: 1000,--}}
+{{--        slidesPerView: 1,--}}
+{{--        parallax: true,--}}
+{{--        // centeredSlides: true,--}}
+{{--        autoplay: {--}}
+{{--            delay: 3000,--}}
+{{--            disableOnInteraction: false,--}}
+{{--        },--}}
+{{--        pagination: {--}}
+{{--            el: ".showcaseSlider-pagination",--}}
+{{--            clickable: true,--}}
+{{--        },--}}
+{{--        navigation: {--}}
+{{--            nextEl: '.showcaseSlider-next',--}}
+{{--            prevEl: '.showcaseSlider-prev',--}}
+{{--        },--}}
+{{--    });--}}
+{{--</script>--}}
