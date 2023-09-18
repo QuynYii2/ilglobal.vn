c537b4b6f6c88622bd9c3e34b18e12da0b294fa2
tranthingocanh22 <103102505+tranthingocanh22@users.noreply.github.com>
2023-09-18 11:49:22 +0700

    giao dien

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -1,6 +1,6 @@
 <!-- ======= Header ======= -->
 <header id="header" class="header align-items-center fixed-top">
-    <div class="header-desktop container-fluid container-xl align-items-center justify-content-between">
+    <div class="header-desktop container-fluid align-items-center justify-content-between">
         <a href="{{route('index')}}" class="logo d-flex align-items-center">
             <h1>Logis</h1>
         </a>
@@ -32,57 +32,6 @@
                 <li><a class="get-a-quote" href="#">Get a Quote</a></li>
             </ul>
         </nav><!-- .navbar -->
-    </div>
-    <div class="header-mobile">
-        <div class="d-flex justify-content-between header-mobile--top">
-            <div class="hd-desktop--left">
-                <a href="{{route('index')}}" class="logo d-flex align-items-center">
-                    <h1>Logis</h1>
-                </a>
-            </div>
-            <div class="hd-desktop--right d-flex justify-content-end">
-                <button onclick="openMenu()"><i class="fa-solid fa-bars"></i></button>
-            </div>
-        </div>
-        <div class="nav_menu" id="demo">
-            <div class="nav_menu--header d-flex justify-content-between">
-                <div onclick="closeMenu()" class="header-left">
-                    <i class="fa-solid fa-xmark"></i>
-                </div>
-                <div class="header-right">
-                    <a href=""><i class="fa-solid fa-house"></i></a>
-                </div>
-            </div>
-            <div class="nav_menu--body">
-                <div class="list-menu">
-                    <div class="menu-item"><a href="#"><p>News</p></a></div>
-                    <div class="menu-item"><a href="#"><p>Mentoring</p></a></div>
-                    <div class="menu-item"><a href="#"><p>Education Training</p></a></div>
-                    <div class="menu-item"><a href="#"><p>Clinic</p></a></div>
-                    <div class="menu-item"><a href="#"><p>Recruitment</p></a></div>
-                    <div class="menu-item"><a href="#"><p>Selling buying</p></a></div>
-                </div>
-            </div>
-        </div>
-        <div class="nav_search" id="nav_search">
-            <div class="nav_search--header d-flex justify-content-between">
-                <div onclick="closeSearch()" class="header-left">
-                    <i class="fa-solid fa-xmark"></i>
-                </div>
-            </div>
-            <div class="nav_search--body">
-                <div class="searchbox">
-                    <div class="input-group">
-                        <div class="form-outline">
-                            <input type="search" id="form1" class="form-control" placeholder="Please enter a search term." />
-                        </div>
-                        <button type="button" class="btn">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </header><!-- End Header -->
 
